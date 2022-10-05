3a00afe1469f9fe0f947af44cee45bafbb5afb4c
Deflandrenassim <nassim.def@gmail.com>
2022-10-05 07:32:08 +0200

    features: add librayrie loading

--- src/container/PageEngland/England.jsx
+++ src/container/PageEngland/England.jsx
@@ -1,54 +1,58 @@
 import React from 'react'
 import axios from 'axios';
-import {useEffect, useState} from 'react';
-import { Contains, ContainsCard, ContainsInformation, ContainsPicture, ContainsName, 
+import { useEffect, useState } from 'react';
+import { Loading } from '../../components/loading/Loading';
+import {
+  Contains, ContainsCard, ContainsInformation, ContainsPicture, ContainsName,
   ContainsScore
 } from '../../components/Contains/Contains';
 import './England.css';
 
 export const England = () => {
-    const [apiMatch , setApiMatch] = useState(null);
-    useEffect(() => {
-      axios.get('http://localhost:8080/premiereleague?league=premiereleague')
-      .then((response ) => {
+  const [apiMatch, setApiMatch] = useState(null);
+  useEffect(() => {
+    axios.get('http://localhost:8080/premiereleague?league=premiereleague')
+      .then((response) => {
         console.log(response);
         setApiMatch(response.data);
       })
-        
-       }, []);
-    return (
-      <div className="App">
-        
-        {apiMatch ?
-          ( 
-            <Contains>
-            <img src={apiMatch.sports_results.thumbnail} alt="premiere league"/>
-  
-          <h1>{apiMatch.sports_results.title}</h1>
-            {apiMatch.sports_results.games.map(( game ) => (
+
+  }, []);
+  return (
+    <div className="App">
+
+      {apiMatch ?
+        (
+          <Contains>
+            <img src={apiMatch.sports_results.thumbnail} alt="premiere league" />
+
+            <h1>{apiMatch.sports_results.title}</h1>
+            {apiMatch.sports_results.games.map((game) => (
               <ContainsCard>
                 {
-                game.teams.map(( team ) => (
-                    <ContainsInformation> 
-                    <ContainsPicture src={team.thumbnail} alt={team.name}/>
-                    <ContainsName> {team.name}</ContainsName>
-                    <ContainsScore> {team.score} </ContainsScore>
-                   </ContainsInformation>
-                 
-                ))}
+                  game.teams.map((team) => (
+                    <ContainsInformation>
+                      <ContainsPicture src={team.thumbnail} alt={team.name} />
+                      <ContainsName> {team.name}</ContainsName>
+                      <ContainsScore> {team.score} </ContainsScore>
+                    </ContainsInformation>
+
+                  ))}
               </ContainsCard>
             )
-          )   
-                }
+            )
+            }
           </Contains>
-          )
-          : 
-          (
-          <div> No games ce week-end </div> 
-          )
-        }
-      
-      </div>
-    );
+        )
+        :
+        (
+          <div>
+            <Loading />
+          </div>
+        )
+      }
+
+    </div>
+  );
 }
 
--- src/container/PageFrench/French.jsx
+++ src/container/PageFrench/French.jsx
@@ -1,53 +1,57 @@
 import React from 'react'
 import axios from 'axios';
-import {useEffect, useState} from 'react';
-import { Contains, ContainsCard, ContainsInformation, ContainsPicture, ContainsName, 
+import { useEffect, useState } from 'react';
+import {
+  Contains, ContainsCard, ContainsInformation, ContainsPicture, ContainsName,
   ContainsScore
 } from '../../components/Contains/Contains';
+import { Loading } from '../../components/loading/Loading';
 import './French.css';
 
 export const French = () => {
-    const [apiMatchFrench , setApiMatchFrench] = useState(null);
-    useEffect(() => {
-      axios.get('http://localhost:8080/ligue1?league=ligue1')
-      .then((response ) => {
+  const [apiMatchFrench, setApiMatchFrench] = useState(null);
+  useEffect(() => {
+    axios.get('http://localhost:8080/ligue1?league=ligue1')
+      .then((response) => {
         console.log(response);
         setApiMatchFrench(response.data);
       })
-        
-       }, []);
-    return (
-      <div className="App">
-        
-      {apiMatchFrench ?
-        ( 
-          <Contains >
-          <img src={apiMatchFrench.sports_results.thumbnail} alt="premiere league"/>
 
-        <h1>{apiMatchFrench.sports_results.title}</h1>
-          {apiMatchFrench.sports_results.games.map(( game ) => (
-            <ContainsCard>
-              {
-              game.teams.map(( team ) => (
-                  <ContainsInformation appareance="appareance"> 
-                  <ContainsPicture src={team.thumbnail} alt={team.name}/>
-                  <ContainsName> {team.name}</ContainsName>
-                  <ContainsScore> {team.score} </ContainsScore>
-                 </ContainsInformation>
-               
-              ))}
-            </ContainsCard>
-          )
-        )   
-              }
-        </Contains>
-        )
-        : 
+  }, []);
+  return (
+    <div className="App">
+
+      {apiMatchFrench ?
         (
-        <div> loading... </div> 
+          <Contains >
+            <img src={apiMatchFrench.sports_results.thumbnail} alt="premiere league" />
+
+            <h1>{apiMatchFrench.sports_results.title}</h1>
+            {apiMatchFrench.sports_results.games.map((game) => (
+              <ContainsCard>
+                {
+                  game.teams.map((team) => (
+                    <ContainsInformation appareance="appareance">
+                      <ContainsPicture src={team.thumbnail} alt={team.name} />
+                      <ContainsName> {team.name}</ContainsName>
+                      <ContainsScore> {team.score} </ContainsScore>
+                    </ContainsInformation>
+
+                  ))}
+              </ContainsCard>
+            )
+            )
+            }
+          </Contains>
+        )
+        :
+        (
+          <div>
+            <Loading />
+          </div>
         )
       }
-    
-    </div>
-    );
+
+    </div >
+  );
 }
